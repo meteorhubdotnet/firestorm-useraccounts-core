339abf0b91991526a3e0421af1c49c03e6f84ea0
Yacine Merzouk <yacine@merzouk.ca>
2022-08-20 22:04:40 -0400

    1.0.10 Updated package dependencies

--- package.js
+++ package.js
@@ -2,7 +2,7 @@
 
 Package.describe({
   summary: 'Fork of useraccounts:core for Firestorm',
-  version: '1.0.7',
+  version: '1.0.10',
   name: 'meteorhubdotnet:firestorm-useraccounts-core',
   git: 'https://github.com/meteorhubdotnet/firestorm-useraccounts-core',
 });
